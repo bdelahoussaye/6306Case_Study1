efc44ba28a0e5e3c7186eafefea119044d93df71
sac3tf <cockes@smu.edu>
2018-06-19 18:43:22 -0500

    Updated to answer questions 5 and 6

--- MergeFiles/MergeFiles.R
+++ MergeFiles/MergeFiles.R
@@ -1,7 +1,18 @@
+##Read in the Files
 Beer <- read.csv(file="/Users/stevencocke/Downloads/CaseStudy_2_2_2/Beers.csv", header=TRUE, sep=",")
 Breweries <-read.csv(file="/Users/stevencocke/Downloads/CaseStudy_2_2_2/Breweries.csv", header=TRUE, sep=",")
 
+##Merge the files and assign to variable
 BeerBreweries <- merge(Beer, Breweries, by.x="Brewery_id", by.y="Brew_ID", all=TRUE)
-BeerBreweries
 
-write.csv(BeerBreweries, "/Users/stevencocke/Desktop/DoingDataScience Project/BeerBreweries.csv", row.names=FALSE)
+##Write to a csv
+write.csv(BeerBreweries, "/Users/stevencocke/Desktop/6306Case_Study1/BeerBreweries.csv", row.names=FALSE)
+
+##Which state has the highest ABV?
+BeerBreweries[which(BeerBreweries$ABV == max(BeerBreweries$ABV, na.rm=TRUE)),10]
+
+##Which state has the most bitter beer?
+BeerBreweries[which(BeerBreweries$IBU == max(BeerBreweries$IBU, na.rm=TRUE)),10]
+
+##Summary statistics for the ABV variable
+summary(BeerBreweries$ABV)
