aca49f3189ffeb0611079b582a5aff94791c7e84
tjj@smu.edu <tjj@smu.edu>
2018-06-19 19:23:06 -0500

    code updates

--- TotalByState.R
+++ TotalByState.R
@@ -1,3 +1,11 @@
 #Total Breweries by state
+# Read in the breweries data
 breweries <- read.csv("breweries.csv", header  = TRUE)
-aggregate( rep(1,dim(breweries)[1]), by=list(breweries$State), sum)
+# compile the number by state using the aggregate function in R
+breweries.by.State <- aggregate( rep(1,dim(breweries)[1]), by=list(breweries$State), sum)
+# Update the column names to something more meaningful
+colnames(breweries.by.State) <- c("State","Number")
+# Sort by state from largest to smallest number of breweries
+sortedBreweriesByState <- breweries.by.State[order(breweries.by.State$Number, decreasing = TRUE),]
+# Display the Brewer's by States in descending order
+sortedBreweriesByState
