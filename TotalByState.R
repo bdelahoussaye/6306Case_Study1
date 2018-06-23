6daa48c6876efb9cacb98db3bd05896c6c5123fe
tjj@smu.edu <tjj@smu.edu>
2018-06-23 06:45:38 -0500

    Updated the #of Breweries by state by adding a barchart for easier viewing

--- TotalByState.R
+++ TotalByState.R
@@ -1,4 +1,16 @@
+######################################################################
 #Total Breweries by state
+# Writen for Doing Data Science MidTerm  Summer 2018
+# Authors: Terry Johnson, Brandon de la Houssaya, Steven Cocke
+# Input files are "breweries.csv" and "beers.csv"
+#
+# Computes the total breweries by state in sorted order
+# displays top 6 and barchart by state
+# 
+# Updated Log
+# 6/13/2018    TJ  Initial
+# 6/23/2018    TJ Added Bar chart
+#####################################################################
 # Read in the breweries data
 breweries <- read.csv("breweries.csv", header  = TRUE)
 # compile the number by state using the aggregate function in R
@@ -7,5 +19,7 @@ breweries.by.State <- aggregate( rep(1,dim(breweries)[1]), by=list(breweries$Sta
 colnames(breweries.by.State) <- c("State","Number")
 # Sort by state from largest to smallest number of breweries
 sortedBreweriesByState <- breweries.by.State[order(breweries.by.State$Number, decreasing = TRUE),]
-# Display the Brewer's by States in descending order
-sortedBreweriesByState
+# Display the Breweries by States in descending order top 6
+head(sortedBreweriesByState,6)
+# DIsplay as BarChart for easier viewing of a large data set
+barplot(sortedBreweriesByState[,2], beside = TRUE, names.arg = sortedBreweriesByState$State,ylab="# Breweries", main = "Brweries By State", xlab = "State")
